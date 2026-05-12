a6364dbc4414aaa632b61850d5c67a77ec9bc0d6
renkanaeru71 <rizkypra1156@gmail.com>
2026-05-12 21:02:37 +0700

    apin memek

--- niger.cpp
+++ niger.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<iomanip>
 using namespace std;
-int main(){
-    cout<<"niger"<<endl;
-}
+
+const int Max_Anime = 100;
+const int Max_Genre = 50;
+const char namafile[] = "anime_data.txt";
+
+struct anime{
+    int id;
+    char title[100];
+    char genre[Max_Genre][30];
+    int eps;
+    float rating;
+    int tahun;
+    char status[20]; // Ongoing, Completed, Upcoming
+};
